6cd7723867a54d3ced9d3c9c25f07404f6585e8c
li-LenovoThinkBook14-desktop <123456789@gmail.com>
2026-09-14 20:33:43 +0800

    Make Project 5 tests runnable from repository root

--- projects/project5_vla_action_diffusion/tests/test_project5.py
+++ projects/project5_vla_action_diffusion/tests/test_project5.py
@@ -1,4 +1,9 @@
+import sys
+from pathlib import Path
+
 import torch
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 from eval import evaluate
 from model import DiffusionPolicy, VisionEncoder
